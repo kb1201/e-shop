35285160efc062002e8f1f4f34491f0cc12de7ce
Katarina Bošnjak <katarina.bosnjak@t.ht.hr>
2026-05-27 12:22:28 +0200

    Remove redundant credential check in AuthService.login() #6

--- user/src/main/java/hr/fer/dipl/service/AuthService.java
+++ user/src/main/java/hr/fer/dipl/service/AuthService.java
@@ -1,6 +1,5 @@
 package hr.fer.dipl.service;
 
-
 import hr.fer.dipl.db.model.User;
 import hr.fer.dipl.dto.LoginResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,19 +17,16 @@ public class AuthService {
 
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
 
     public LoginResponse login(String username, String password) {
-        User user = (User) userDetailsService.loadUserByUsername(username);
-
-        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
 
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(username, password);
+
         authenticationManager.authenticate(authenticationToken);
+
+        User user = (User) userDetailsService.loadUserByUsername(username);
 
         List<String> roles = user.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
